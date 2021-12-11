485fe07cb71820df251c432fd4913d1385e4873e
GhostlyCrowd <jeffjiggens@gmail.com>
2021-12-10 20:49:42 -0500

    Forgot -y On the Python install bits.

--- pi-hole5.sh
+++ pi-hole5.sh
@@ -30,7 +30,7 @@ echo "=============================="
 echo "Installing Python3 setting up virtual env and installing Certbot-nginx To Get SSL From Let's Encrypt"
 echo "=============================="
 echo ""
-sudo apt install python3 python3-venv libaugeas0
+sudo apt-get -y install python3 python3-venv libaugeas0
 sudo python3 -m venv /opt/certbot/
 sudo /opt/certbot/bin/pip install --upgrade pip
 sudo /opt/certbot/bin/pip install certbot certbot-nginx
